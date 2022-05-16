5e0e1efac0c86e16f2b59de8986203f41ec27ee5
Alex <petenkov2822@gmail.com>
2022-05-17 00:04:59 +0400

    All releases done

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const generateNames = require('./generateNames')
 const names = generateNames(3);
-console.log(names)
+//console.log(names)
 
 const getRandomInteger = require('./getRandomInteger')
-let randomNum = getRandomInteger()
-const numberOfNames = process.argv[2] || randomNum
-console.log(generateNames(numberOfNames));
+const numberOfNames = process.argv[2] || getRandomInteger()
+//console.log(generateNames(numberOfNames));
 
 const fs = require('fs');
 fs.writeFileSync(`results/${numberOfNames}-names.txt`, `${generateNames(numberOfNames)}`);
